25f447fbab8cf1f426b1a524ff842e695f48df82
soumadip-dev <soumadipmajila@gmail.com>
2025-07-20 12:00:00 +0530

    How to Add Elements to an Array in JS?

--- JS-practise/Array.js
+++ JS-practise/Array.js
@@ -15,3 +15,17 @@ console.log(salad[0], fruits[2]);
 for (let i = 0; i <= salad.length - 1; i++) {
   console.log(salad[i]);
 }
+
+//* ✅ How to Add Elements to an Array in JS?
+
+// 1. push() - adds elements to the end of an array and returns the new length of the array
+const puRet = salad.push('🥜');
+console.log(puRet); // 8
+console.log(salad); // array after adding element at the end
+
+// 2. unshift() - adds elements to the beginning of an array and returns the new length of the array
+const unRet = salad.unshift('🍒');
+console.log(unRet); // 9
+console.log(salad); // array after adding element at the beginning
+
+// - push() and unshift() both methods mutate the original array.
