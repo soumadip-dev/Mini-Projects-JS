6eb3cd2d7795996797e937ba5f287fc70016bf92
soumadip-dev <soumadipmajila@gmail.com>
2025-07-25 12:00:00 +0530

    How to Get Elements from an Array in JS?

--- JS-practise/Array.js
+++ JS-practise/Array.js
@@ -10,4 +10,8 @@ console.log(salad, fruits);
 // NOTE: If we provide a single number argument to the Array constructor, it creates an array with that length.
 const numArr = new Array(3); // [empty × 3]
 
-
+//* ✅ How to Get Elements from an Array in JS?
+console.log(salad[0], fruits[2]);
+for (let i = 0; i <= salad.length - 1; i++) {
+  console.log(salad[i]);
+}
