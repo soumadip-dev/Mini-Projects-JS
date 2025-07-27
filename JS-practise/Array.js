816cf7df787c4e7e0cb9a793ad0b43772bc2f8a9
soumadip-dev <soumadipmajila@gmail.com>
2025-07-27 12:00:00 +0530

    How to Create an Array in JavaScript?

--- JS-practise/Array.js
+++ JS-practise/Array.js
@@ -1,1 +1,13 @@
 // JS ARRAY
+
+//* ✅ How to Create an Array in JavaScript?
+// 1. Array Literal
+const salad = ['🍅', '🍄', '🥦', '🥒', '🌽', '🥕', '🥑'];
+// 2. Array constructor
+const fruits = new Array('🍈', '🍉', '🍊', '🍋', '🍌', '🍍', '🥭');
+console.log(salad, fruits);
+
+// NOTE: If we provide a single number argument to the Array constructor, it creates an array with that length.
+const numArr = new Array(3); // [empty × 3]
+
+
